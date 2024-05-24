90e9f3e99d7291d33f69a8bfd4218f787a38ff66
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-05-24 20:16:24 +0100

    test: added test for mul, div and pow of parser

--- rpn/parser_test.go
+++ rpn/parser_test.go
@@ -10,7 +10,7 @@ type Number interface {
 	int | float64
 }
 
-const Epsion = 0.0001
+const Epsilon = 0.0001
 
 func approxEq(x, y, eps float64) bool {
 	diff := x - y
@@ -23,6 +23,18 @@ func approxEq(x, y, eps float64) bool {
 	}
 
 	return false
+}
+
+func approxEqStack(stack1, stack2 []float64) bool {
+	if len(stack1) != len(stack2) {
+		return false
+	}
+	for i := 0; i < len(stack1); i++ {
+		if ok := approxEq(stack1[i], stack2[i], Epsilon); !ok {
+			return false
+		}
+	}
+	return true
 }
 
 func TestStringParser_ParsesANumberCorrectly(t *testing.T) {
@@ -104,13 +116,13 @@ func TestStringParser_ParsesANumberCorrectly(t *testing.T) {
 	}
 }
 
-func TestStringParser_Sum(t *testing.T) {
+func TestStringParserSum_AddsTwoElements(t *testing.T) {
 	t.Parallel()
 	type TestCase struct {
 		Input  string
 		Output []float64
 	}
-	testCasesInt := []TestCase{
+	testCases := []TestCase{
 		{
 			Input:  "42532454 2 +",
 			Output: []float64{42532456.0},
@@ -159,8 +171,12 @@ func TestStringParser_Sum(t *testing.T) {
 			Input:  "0.0000000001 1000000000000000 +",
 			Output: []float64{1000000000000000.0000000001},
 		},
+		{
+			Input:  "23 42		+15    ",
+			Output: []float64{65, 15},
+		},
 	}
-	for _, tc := range testCasesInt {
+	for _, tc := range testCases {
 		stack := rpn.NewStack()
 		err := rpn.StringParser(stack, tc.Input)
 		if err != nil {
@@ -169,19 +185,19 @@ func TestStringParser_Sum(t *testing.T) {
 		want := tc.Output
 		got := stack.GetValues()
 
-		if !cmp.Equal(want, got) {
+		if !approxEqStack(want, got) {
 			t.Error(cmp.Diff(want, got))
 		}
 	}
 }
 
-func TestStringParser_Diff(t *testing.T) {
+func TestStringParserDiff_SubtractsTwoElements(t *testing.T) {
 	t.Parallel()
 	type TestCase struct {
 		Input  string
 		Output []float64
 	}
-	testCasesInt := []TestCase{
+	testCases := []TestCase{
 		{
 			Input:  "42532454 2 -",
 			Output: []float64{42532452.0},
@@ -230,21 +246,231 @@ func TestStringParser_Diff(t *testing.T) {
 			Input:  "0.0000000001 1000000000000000 -",
 			Output: []float64{-999999999999999.9999999999},
 		},
+		{
+			Input:  "23 42		-15   ",
+			Output: []float64{-19, 15},
+		},
 	}
-	for _, tc := range testCasesInt {
+	for _, tc := range testCases {
 		stack := rpn.NewStack()
 		err := rpn.StringParser(stack, tc.Input)
 		if err != nil {
 			t.Fatal(err)
 		}
-		want := tc.Output[0]
-		got := stack.GetValues()[0]
+		want := tc.Output
+		got := stack.GetValues()
 
-		// if !cmp.Equal(want, got) {
-		// 	t.Error(cmp.Diff(want, got))
-		// }
-		if !approxEq(want, got, Epsion) {
-			t.Errorf("want %f, got %f", want, got)
+		if !approxEqStack(want, got) {
+			t.Error(cmp.Diff(want, got))
+		}
+	}
+}
+
+func TestStringParserMul_MultipliesTwoElements(t *testing.T) {
+	t.Parallel()
+	type TestCase struct {
+		Input  string
+		Output []float64
+	}
+	testCases := []TestCase{
+		{
+			Input:  "42532454 2 *",
+			Output: []float64{85064908},
+		},
+		{
+			Input:  "005432 432 *",
+			Output: []float64{2346624},
+		},
+		{
+			Input:  "      5432 0 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "5432     000542345*",
+			Output: []float64{2946018040},
+		},
+		{
+			Input:  "0 0 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0.14 5.29 *",
+			Output: []float64{0.7406},
+		},
+		{
+			Input:  "0 0.0000000 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0.00000 0.0000000 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0 1 - 2.3 *",
+			Output: []float64{-2.3},
+		},
+		{
+			Input:  "0 3.14 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "3.14 0 *",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0 0.0000000001 - 1000000000000000 *",
+			Output: []float64{-100000.0},
+		},
+		{
+			Input:  "23 42		*15   ",
+			Output: []float64{966, 15},
+		},
+	}
+	for _, tc := range testCases {
+		stack := rpn.NewStack()
+		err := rpn.StringParser(stack, tc.Input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := tc.Output
+		got := stack.GetValues()
+
+		if !approxEqStack(want, got) {
+			t.Error(cmp.Diff(want, got))
+		}
+	}
+}
+
+func TestStringParserDiv_DividesTwoElements(t *testing.T) {
+	t.Parallel()
+	type TestCase struct {
+		Input  string
+		Output []float64
+	}
+	testCases := []TestCase{
+		{
+			Input:  "42532454 2 /",
+			Output: []float64{21266227},
+		},
+		{
+			Input:  "005432 432 /",
+			Output: []float64{12.574074074074074},
+		},
+		{
+			Input:  "    0  5432  /",
+			Output: []float64{0},
+		},
+		{
+			Input:  "5432     000542345/",
+			Output: []float64{0.010015764872912999},
+		},
+		{
+			Input:  "0 0.1 -  0.1 /",
+			Output: []float64{-1},
+		},
+		{
+			Input:  "0.14 5.29 /",
+			Output: []float64{0.026465028355387527},
+		},
+		{
+			Input:  "0 0.00000001 /",
+			Output: []float64{0},
+		},
+		{
+			Input:  " 2.3 1/",
+			Output: []float64{2.3},
+		},
+		{
+			Input:  "1 3.14 /",
+			Output: []float64{0.3184713375796178},
+		},
+		{
+			Input:  "3.14 3.14 /",
+			Output: []float64{1},
+		},
+		{
+			Input:  " 1000000000000000 0.0001 /",
+			Output: []float64{1e+19},
+		},
+		{
+			Input:  "23 42		/15   ",
+			Output: []float64{0.5476190476190477, 15},
+		},
+	}
+	for _, tc := range testCases {
+		stack := rpn.NewStack()
+		err := rpn.StringParser(stack, tc.Input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := tc.Output
+		got := stack.GetValues()
+
+		if !approxEqStack(want, got) {
+			t.Error(cmp.Diff(want, got))
+		}
+	}
+}
+
+func TestStringParserPow_PowersTwoElements(t *testing.T) {
+	t.Parallel()
+	type TestCase struct {
+		Input  string
+		Output []float64
+	}
+	testCases := []TestCase{
+		{
+			Input:  "42532454 2 ^",
+			Output: []float64{1809009643262116},
+		},
+		{
+			Input:  "005432 0.01 ^",
+			Output: []float64{1.0898070112786702},
+		},
+		{
+			Input:  "      5432 0 ^",
+			Output: []float64{1},
+		},
+		{
+			Input:  "0     000542345^",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0.14 5.29 ^",
+			Output: []float64{3.041006222083085e-05},
+		},
+		{
+			Input:  "0 1 - 2 ^",
+			Output: []float64{1},
+		},
+		{
+			Input:  "0 1 - 3 ^",
+			Output: []float64{-1},
+		},
+		{
+			Input:  "0 3.14 ^",
+			Output: []float64{0},
+		},
+		{
+			Input:  "0 3.14 - 0 ^",
+			Output: []float64{1},
+		},
+		{
+			Input:  "2 42		^15   ",
+			Output: []float64{4398046511104, 15},
+		},
+	}
+	for _, tc := range testCases {
+		stack := rpn.NewStack()
+		err := rpn.StringParser(stack, tc.Input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := tc.Output
+		got := stack.GetValues()
+
+		if !approxEqStack(want, got) {
+			t.Error(cmp.Diff(want, got))
 		}
 	}
 }
