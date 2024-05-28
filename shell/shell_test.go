1864f3f31eff9002ade3d65295547850e9d6aa68
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-05-28 20:30:55 +0100

    test: added test for history navigation

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -2,6 +2,7 @@ package shell_test
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/azr4e1/polacco/shell"
@@ -228,7 +229,7 @@ func TestShellRun_StoresHistoryCorrectly(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	inputStr := "3 1 2 +  \npop\np\nh\nl  \nls\n^\n-"
+	inputStr := "3 1 2 +  \npop\np\nh\nl  \nls\n^\n-\n-\n	\n-\n	\n"
 	_, err = input.Write([]byte(inputStr))
 	if err != nil {
 		t.Error(err)
@@ -239,5 +240,160 @@ func TestShellRun_StoresHistoryCorrectly(t *testing.T) {
 
 	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestShellRun_MaintainsTheCorrectHistoryLength(t *testing.T) {
+	t.Parallel()
+	input := new(bytes.Buffer)
+	output := new(bytes.Buffer)
+	error := new(bytes.Buffer)
+	maxLength := 100
+	session, err := shell.NewSession(
+		shell.SetStdin(input),
+		shell.SetStdout(output),
+		shell.SetStderr(error),
+		shell.SetMaxHistorySize(maxLength),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	inputStr := "pop"
+	for i := 0; i < maxLength; i++ {
+		inputStr += fmt.Sprintf("\nls%d", i)
+	}
+	_, err = input.Write([]byte(inputStr))
+	if err != nil {
+		t.Error(err)
+	}
+	want := []string{}
+	for i := 0; i < maxLength; i++ {
+		want = append(want, fmt.Sprintf("ls%d", i))
+	}
+
+	session.Run()
+	got := session.GetHistory()
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestShellRunGetPrevHistoryElement_ReturnsTheCorrectElements(t *testing.T) {
+	t.Parallel()
+	input := new(bytes.Buffer)
+	output := new(bytes.Buffer)
+	error := new(bytes.Buffer)
+	session, err := shell.NewSession(
+		shell.SetStdin(input),
+		shell.SetStdout(output),
+		shell.SetStderr(error),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	inputStr := "3 1 2 +  \npop\np\nh\nl  \nls\n^\n-"
+	_, err = input.Write([]byte(inputStr))
+	if err != nil {
+		t.Error(err)
+	}
+	want := "-"
+	session.Run()
+	got, err := session.GetPrevHistoryElement()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestShellRunGetNextHistoryElement_ReturnsTheCorrectElements(t *testing.T) {
+	t.Parallel()
+	input := new(bytes.Buffer)
+	output := new(bytes.Buffer)
+	error := new(bytes.Buffer)
+	session, err := shell.NewSession(
+		shell.SetStdin(input),
+		shell.SetStdout(output),
+		shell.SetStderr(error),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	inputStr := "3 1 2 +  \npop\np\nh\nl  \nls\n^\n-"
+	_, err = input.Write([]byte(inputStr))
+	if err != nil {
+		t.Error(err)
+	}
+	want := "-"
+	session.Run()
+	_, err = session.GetPrevHistoryElement()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = session.GetPrevHistoryElement()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := session.GetNextHistoryElement()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
+
+func TestShellRunGetPrevHistoryElement_ReturnsError(t *testing.T) {
+	t.Parallel()
+	input := new(bytes.Buffer)
+	output := new(bytes.Buffer)
+	error := new(bytes.Buffer)
+	session, err := shell.NewSession(
+		shell.SetStdin(input),
+		shell.SetStdout(output),
+		shell.SetStderr(error),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	inputStr := "3 1 2 +  "
+	_, err = input.Write([]byte(inputStr))
+	if err != nil {
+		t.Error(err)
+	}
+	session.Run()
+	_, err = session.GetPrevHistoryElement()
+	_, err = session.GetPrevHistoryElement()
+	if err == nil {
+		t.Error("want error, got nil")
+	}
+}
+
+func TestShellRunGetNextHistoryElement_ReturnsError(t *testing.T) {
+	t.Parallel()
+	input := new(bytes.Buffer)
+	output := new(bytes.Buffer)
+	error := new(bytes.Buffer)
+	session, err := shell.NewSession(
+		shell.SetStdin(input),
+		shell.SetStdout(output),
+		shell.SetStderr(error),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	inputStr := "3 1 2 +  \npop\np\nh\nl  \nls\n^\n-"
+	_, err = input.Write([]byte(inputStr))
+	if err != nil {
+		t.Error(err)
+	}
+	session.Run()
+	_, err = session.GetNextHistoryElement()
+	if err == nil {
+		t.Error("want error, got nil")
 	}
 }
