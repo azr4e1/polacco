e32f387da4c9f4cb59760899487b5af4a22c882f
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-06-09 21:02:35 +0100

    feat(ui): added style to output

--- ui/ui.go
+++ ui/ui.go
@@ -9,6 +9,7 @@ import (
 	"github.com/azr4e1/polacco/rpn"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 const TAB = "\t"
@@ -19,8 +20,9 @@ type model struct {
 	rl            readline.Model
 	stack         *rpn.RPNStack
 	currentOutput string
-	repl          bool
+	outputStyle   lipgloss.Style
 	help          string
+	history       string
 }
 
 func (m model) Init() tea.Cmd {
@@ -28,9 +30,8 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) View() string {
-	output := m.rl.View()
-	// output += fmt.Sprintf("\n%v", m.stack.GetValues())
-	output += fmt.Sprintf("\n%s", m.currentOutput)
+	output := m.rl.View() + "\n"
+	output += m.outputStyle.Inline(true).Render(m.currentOutput)
 
 	return output
 }
@@ -39,11 +40,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, DefaultKeyMap.Clear):
-			if m.repl {
-				return m, tea.ClearScreen
-			}
-
 		case key.Matches(msg, DefaultKeyMap.Quit):
 			return m, tea.Quit
 		}
@@ -62,8 +58,9 @@ func initialModel() tea.Model {
 	stack := rpn.NewStack()
 	rl := readline.New()
 	return model{
-		stack: stack,
-		rl:    rl,
+		stack:       stack,
+		rl:          rl,
+		outputStyle: lipgloss.NewStyle().Italic(true).Foreground(lipgloss.Color("#71797E")),
 	}
 }
 
@@ -73,14 +70,14 @@ func (m *model) actionParse(input string) {
 	// case "h", "he", "hel", "help":
 	// 	s.Help()
 	case "l", "ls", "li", "lis", "list":
-		m.currentOutput = fmt.Sprintf("%v", m.stack.GetValues())
+		m.currentOutput = fmt.Sprintf("%v\n", m.stack.GetValues())
 	case "p", "po", "pop":
 		val, err := m.stack.Pop()
 		if err != nil {
 			m.currentOutput = fmt.Sprintln("error:", err)
 			return
 		}
-		m.currentOutput = fmt.Sprintf("%f", val)
+		m.currentOutput = fmt.Sprintf("%f\n", val)
 	case "r", "re", "res", "rese", "reset":
 		m.stack = rpn.NewStack()
 		m.currentOutput = ""
